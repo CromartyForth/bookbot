2253f106570bbbf453b408b52e76312117becbe8
CromartyForth <glenn@gcampbell.co.uk>
2025-07-08 17:55:30 +0100

    add command line arguments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
 from stats import get_num_words, get_letter_count, get_sorted_dictionary
-path = "books/frankenstein.txt"
 
 def get_book_text(path):
     with open(f"./{path}") as f:
@@ -8,20 +8,28 @@ def get_book_text(path):
 
 
 def main():
-    text = get_book_text(path)
-    num_words = get_num_words(text)
-    letter_count = get_letter_count(text)
-    sorted_dictionary = get_sorted_dictionary(letter_count)
-    
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {path}...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for entry in sorted_dictionary:
-        if entry["char"].isalpha():
-            print(f"{entry["char"]}: {entry["num"]}")
-    print("============= END ===============")
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+
+        path = sys.argv[1]
+
+        text = get_book_text(path)
+        num_words = get_num_words(text)
+        letter_count = get_letter_count(text)
+        sorted_dictionary = get_sorted_dictionary(letter_count)
+        
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for entry in sorted_dictionary:
+            if entry["char"].isalpha():
+                print(f"{entry["char"]}: {entry["num"]}")
+        print("============= END ===============")
 
 
 
